fcfff6987c48c32d415a424c136f231b7e99a9a2
papercompute <papercompute@gmail.com>
2014-08-21 23:19:42 +0400

    update

--- game.js
+++ game.js
@@ -1,52 +1,87 @@
 var game={
 l : [
 [
- [0,0,"red"],
- [-1,3,"orange"],
+ [-1,-1,"red",1],
+ [1,0,"orange",1],
  [2,2,"yellow"],
- [-1,-1,"black"],
-// [1,-2,"green"],
+// [-1,1,"green"],
+// [1,-1,"blue"],
 // [-1,-2,"orange"]
 ],
 [
- [1,0,"yellow"],
- [0,1,"gray"],
- [1,2,"yellow"],
-// [-1,-1,"black"],
-// [1,2,"green"],
-// [-1,-2,"orange"]
+ [0,0,"yellow"],
+ [1,1,"gray"],
+ [-1,-1,"yellow"],
+ [-1,1,"black"],
+ [1,-1,"green"],
+ [-2,-2,"orange"],
+ [-2,2,"orange"],
+ [2,-2,"orange"],
 ],
 [
  [1,1,"yellow"],
  [-1,-1,"red"],
  [1,-2,"blue"],
-// [-1,2,"black"],
-// [1,3,"green"],
-// [-1,-2,"orange"]
+ [-1,2,"black"],
+ [1,3,"green"],
+ [-1,-3,"orange"],
+ [0,-3,"blue"],
+ [-1,-2,"black"],
+ [-1,3,"green"],
 ],
 [
+ [0,0,"red"],
  [0,1,"red"],
- [-1,1,"red"],
- [1,-2,"blue"],
-// [-1,2,"black"],
-// [1,3,"green"],
-// [-1,-2,"orange"]
+ [1,0,"blue"],
+ [1,1,"black"],
+ [-1,0,"green"],
+ [0,-1,"orange"],
 ],
 [
+ [0,0,"red"],
+ [0,-1,"red"],
  [0,1,"yellow"],
  [-1,-3,"black"],
  [1,-2,"blue"],
-// [-1,2,"black"],
-// [1,3,"green"],
-// [-1,-2,"orange"]
+ [-1,-2,"black"],
+ [1,-3,"green"],
+ [-1,-2,"orange"],
+ [-1,-2,"black"],
+ [1,-3,"green"],
+ [-1,-2,"orange"],
+],
+[
+ [0,0,"red"],
+ [0,1,"red"],
+ [1,0,"yellow"],
+ [1,1,"black"],
+ [1,2,"blue"],
+ [2,1,"black"],
+ [2,2,"green"],
+ [-1,-2,"orange"],
+ [1,-3,"green"],
+ [-1,-2,"orange"],
+],
+[
+ [2,2,"red"],
+ [2,-1,"red"],
+ [2,-2,"yellow"],
+ [-3,1,"black"],
+ [-1,2,"blue"],
+ [-3,-3,"black"],
+ [-2,-2,"green"],
+ [-1,-2,"orange"],
+ [1,-3,"green"],
+ [-1,-2,"orange"],
 ]
 
 ],
-i:0
+i:6
 };
 
 (function(){
 //console.log(game);
 }());
+
 
 
