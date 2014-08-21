ba99fd72738028c7b77abf38390851400c10221e
papercompute <papercompute@gmail.com>
2014-08-21 11:50:20 +0400

    game

---
+++ game.js
@@ -0,0 +1,35 @@
+var game={
+l : [
+[
+ [0,0,"red"],
+ [-1,3,"orange"],
+ [2,2,"yellow"],
+ [-1,-1,"black"],
+// [1,-2,"green"],
+// [-1,-2,"orange"]
+],
+[
+ [1,0,"yellow"],
+ [0,1,"gray"],
+ [1,2,"yellow"],
+// [-1,-1,"black"],
+// [1,2,"green"],
+// [-1,-2,"orange"]
+],
+[
+ [1,1,"yellow"],
+ [-1,-1,"red"],
+ [1,-2,"blue"],
+// [-1,2,"black"],
+// [1,3,"green"],
+// [-1,-2,"orange"]
+]
+],
+i:0
+};
+
+(function(){
+//console.log(game);
+}());
+
+
