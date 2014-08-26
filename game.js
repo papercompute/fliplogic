e36fc38c25cded3b7684500167c90d679bc55ce0
alex <activedaily@gmail.com>
2014-08-26 15:36:04 +0400

    update

--- game.js
+++ game.js
@@ -1,14 +1,6 @@
 var game={
 l : [
 [ // L0
-/*
- [-1,-1,"red",function(e){
-   var i=e.gindex;  var otop=game.l[game.i][i][1]*100;  var etop=parseInt(e.style.top.slice(0, -1));
-   if(otop == etop){ e.style.top = (etop-100)+"%"; }else{  e.style.top = (otop)+"%"; }
- }],
- [0,0,"orange"],
- [1,1,"yellow"],
-*/
  [0,0,"red"],
  [1,1,"gray",function(e){
    var i=e.gindex;  var oleft=game.l[game.i][i][0]*100; var otop=game.l[game.i][i][1]*100;  var eleft=parseInt(e.style.left.slice(0, -1)); var etop=parseInt(e.style.top.slice(0, -1));
@@ -47,51 +39,6 @@ l : [
    var i=e.gindex;  var oleft=game.l[game.i][i][0]*100; var otop=game.l[game.i][i][1]*100;  var eleft=parseInt(e.style.left.slice(0, -1)); var etop=parseInt(e.style.top.slice(0, -1));
    if(oleft == eleft && otop == etop){ e.style.left = (eleft+100)+"%";  e.style.top = (etop+100)+"%"; }else{ e.style.left = (oleft)+"%";  e.style.top = (otop)+"%"; }
  }],
-],
-[ // L2
- [-2,-3,"gray"],
- [-3,-2,"gray"],
- [-2,-1,"black"],
- [-1,-2,"orange"],
-
- [0,-2,"blue"],
- [1,-1,"blue"],
- [1,-3,"blue"],
- [2,-2,"orange"],
- [3,-1,"orange"],
-
- [-1,0,"gray"],
- [ 0,0,"gray"],
-
-
- [-2,1,"gray"],
- [-3,2,"gray"],
- [-2,3,"black"],
- [-1,1,"orange"],
- [0,1,"orange"],
- [1,1,"gray"],
- [2,2,"gray"],
- [1,3,"black"],
-
-],
-[ // L3
- [-3,-3,"green"],
- [-2,-2,"red"],
- [-2,-1,"red"],
- [-1,-1,"blue"],
- [-1,0,"black"],
- [-1,1,"green"],
- [0,0,"red"],
- [0,1,"red"],
- [1,0,"blue"],
- [1,1,"black"],
- [2,2,"green"],
- [2,1,"orange"],
- [3,3,"orange"],
- [-2,1,"red"],
- [-2,2,"red"],
- [-1,-3,"green"],
- [0,-1,"orange"],
 ],
 [ // L4
  [-2,-2,"red"],
